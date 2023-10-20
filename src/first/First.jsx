6ba2ceca9d7a70bfb760933fe82ca7abfbf8b958
Manab Pokhrel <criroseven39@gmail.com>
2023-10-20 14:39:39 +0545

    test1

--- src/first/First.jsx
+++ src/first/First.jsx
@@ -115,7 +115,10 @@ function First() {
           <span style={{ '--i': 4 }}>e</span>
           <span style={{ '--i': 5 }}>l</span>
         </div>
-  
+        <div id="container">
+          <span id="text1"></span>
+          <span id="text2"></span>
+        </div>
         <button className={styles.button89} style={{ color: "#D70040" }} onClick={handleScroll}>
           SEE PORTFOLIO
         </button>
