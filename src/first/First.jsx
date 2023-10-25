fb13bb90ca56aef5609ed3eaa49d9ba21a6e2d46
Manab Pokhrel <criroseven39@gmail.com>
2023-10-25 18:33:26 +0545

    test5

--- src/first/First.jsx
+++ src/first/First.jsx
@@ -115,7 +115,7 @@ function First() {
           <span style={{ '--i': 4 }}>e</span>
           <span style={{ '--i': 5 }}>l</span>
         </div>
-        <div id="container">
+        <div id="container" className="hide-on-mobile">
           <span id="text1"></span>
           <span id="text2"></span>
         </div>
