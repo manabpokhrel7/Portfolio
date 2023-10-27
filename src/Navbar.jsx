8067fe59f78d17a6af89b43f0960d7efc46a93af
Manab Pokhrel <criroseven39@gmail.com>
2023-10-27 13:28:18 +0545

    Nav

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import './Navbar.css'; 
+import './Navbar.css';
 import React, { useRef } from 'react';
 import First from './first/First';
 import About from './second/About';
@@ -6,43 +6,42 @@ import Services from './third(Services)/Services';
 import Port from './four(My Portfolio)/Port';
 import Contactform from './footer/Contactform';
 import MpIcon from '@mui/icons-material/Mp';
-import ResponsiveAppBar from './ResponsiveAppBar';
-
+import ButtonAppBar from './ResponsiveAppBar';
 
 function Navbar() {
   const aboutRef = useRef(null);
   const portRef = useRef(null);
 
-
+  // Check if the screen width is less than or equal to 768px (adjust this as needed)
+  const isMobile = window.innerWidth <= 768;
 
   return (
     <>
-    <div className="navbar">
-      <div className="logo">
-        <MpIcon style={{ fontSize : '50px' }} />
+      <div className="navbar">
+        <div className="logo">
+          <MpIcon style={{ fontSize: '50px' }} />
+        </div>
+        <div className="menu-toggle" id="menu-toggle">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className="menu" id="menu">
+          <ul>
+            <li>Home</li>
+            <li onClick={() => window.scrollTo({ top: aboutRef.current.offsetTop, behavior: 'smooth' })}>About</li>
+            <li onClick={() => window.scrollTo({ top: portRef.current.offsetTop, behavior: 'smooth' })}>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
-      <div class="menu-toggle" id="menu-toggle">
-    <div class="bar"></div>
-    <div class="bar"></div>
-    <div class="bar"></div>
-  </div>
-      <div className="menu" id='menu'>
-        <ul>
-        <li>Home</li>
-        <li onClick={() => window.scrollTo({ top: aboutRef.current.offsetTop, behavior: 'smooth' })}>About</li>
-        <li onClick={() => window.scrollTo({ top: portRef.current.offsetTop, behavior: 'smooth' })}>Portfolio</li>
-        <li>Contact</li>
-
-        </ul>
-      </div>
-    </div>
-    <ResponsiveAppBar />
-    <First />
-    <div ref={aboutRef}><About /></div>
-    <Services />
-    <div ref={portRef}><Port /></div>
-    <Contactform />
-      </>
+      {isMobile && <ButtonAppBar />} {/* Render ButtonAppBar on mobile */}
+      <First />
+      <div ref={aboutRef}><About /></div>
+      <Services />
+      <div ref={portRef}><Port /></div>
+      <Contactform />
+    </>
   );
 }
 
