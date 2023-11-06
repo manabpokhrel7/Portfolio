f9dc3293d145e64af6f037e5cbe59479d9783311
Manab Pokhrel <criroseven39@gmail.com>
2023-11-06 15:27:20 +0545

    contact

--- src/footer/Contact.jsx
+++ src/footer/Contact.jsx
@@ -1,96 +1,60 @@
 import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import EmailIcon from '@mui/icons-material/Email';
-import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Button from '@mui/material/Button';
-import './Contact.css';
-
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import { useForm, ValidationError } from '@formspree/react';
 function ContactForm() {
-  const inputStyle = {
-    height: '100%',
-  };
-
-  const [state, handleSubmit] = useForm("https://formspree.io/f/maygkdzr"); // Replace with your Formspree endpoint
-
+  const [state, handleSubmit] = useForm("maygkdzr");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+      return <p>Thanks for joining!</p>;
   }
-
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   return (
-    <div className='foot'>
       <form onSubmit={handleSubmit}>
-        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="first-name">
-              First Name
-            </InputLabel>
-            <Input
-              id="first-name"
-              name="first-name"
-              startAdornment={
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              }
-              style={inputStyle}
-            />
-          </FormControl>
-          <TextField
-            id="last-name"
-            name="last-name"
-            label="Last Name"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-            style={inputStyle}
-          />
-          <TextField
-            id="email"
-            type="email"
-            name="email"
-            label="Email"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <EmailIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-            style={inputStyle}
-          /><br></br>
-          <TextareaAutosize
-            id="message"
-            aria-label="message"
-            name="message"
-            placeholder="Your message here"
-            style={inputStyle}
-          /><br></br>
-          <Button variant="contained" type="submit" disabled={state.submitting}>
-            Submit
-          </Button>
-        </Box>
-      </form>
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-    </div>
+       <Box sx={{ width: '100%' }}>
+      <Stack spacing={2}>
+        <Item>REACH OUT TO ME💎</Item><br></br>
+      </Stack>
+    </Box>
+      <input
+      placeholder="Your Email"
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      /><br></br><br></br>
+      
+      <textarea
+      placeholder="Write your message"
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      /><br></br><br></br>
+      <Button variant="outlined" type="submit" disabled={state.submitting}>
+        Submit
+      </Button>
+    </form>
   );
 }
-
 function App() {
-  return <ContactForm />;
+  return (
+    <ContactForm />
+  );
 }
-
 export default App;
