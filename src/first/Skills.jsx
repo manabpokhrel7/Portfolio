c95b6ba33cfd5e6ea72e93f4f6de8a5c833d92bb
Manab Pokhrel <criroseven39@gmail.com>
2023-11-06 15:48:35 +0545

    css

--- src/first/Skills.jsx
+++ src/first/Skills.jsx
@@ -28,81 +28,87 @@ const centerStyle = {
 
 const mobileCentered = {
   width: '100%', // Full width on mobile
-  margin: '0 auto', // Center horizontally on mobile
+  margin: 'auto', // Center horizontally on mobile
 };
-
 export default function HorizontalNonLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState({});
-
-  const totalSteps = () => {
-    return steps.length;
-  };
-
-  const completedSteps = () => {
-    return Object.keys(completed).length;
-  };
-
-  const allStepsCompleted = () => {
-    return completedSteps() === totalSteps();
-  };
-
-  const handleStep = (step) => () => {
-    setActiveStep(step);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-    setCompleted({});
-  };
-
-  return (
-    <Box sx={{
-      ...centerStyle, // Apply common center styling
-      ...(window.innerWidth < 600 ? mobileCentered : null), // Apply mobile styling when the width is less than 600px
-    }}>
-      <Box sx={{ width: '50%', marginTop: '5%' }}>
-        <Stepper nonLinear activeStep={activeStep} alternativeLabel>
-          {steps.map((label, index) => (
-            <Step key={label} completed={completed[index]}>
-              <StepButton
-                color="inherit"
-                onClick={handleStep(index)}
-                icon={
-                  index === 0 ? (
-                    <WebIcon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
-                  ) : index === 1 ? (
-                    <PhotoshopIcon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
-                  ) : index === 2 ? (
-                    <Diversity1Icon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
-                  ) : null
-                }
-              >
-                {label}
-              </StepButton>
-            </Step>
-          ))}
-        </Stepper>
-        <div>
-          {allStepsCompleted() ? (
-            <React.Fragment>
-              <Typography sx={{ mt: 2, mb: 1 }}>
-                All steps completed - you&apos;re finished
-              </Typography>
-              <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                <Box sx={{ flex: '1 1 auto' }} />
-                <Button onClick={handleReset}>Reset</Button>
-              </Box>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-                {stepMessages[activeStep]}
-              </Typography>
-            </React.Fragment>
-          )}
-        </div>
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState({});
+  
+    const totalSteps = () => {
+      return steps.length;
+    };
+  
+    const completedSteps = () => {
+      return Object.keys(completed).length;
+    };
+  
+    const allStepsCompleted = () => {
+      return completedSteps() === totalSteps();
+    };
+  
+    const handleStep = (step) => () => {
+      setActiveStep(step);
+    };
+  
+    const handleReset = () => {
+      setActiveStep(0);
+      setCompleted({});
+    };
+  
+    return (
+      <Box sx={{
+        ...centerStyle, // Apply common center styling
+        ...(window.innerWidth < 600 ? mobileCentered : null), // Apply mobile styling when the width is less than 600px
+      }}>
+        <Box sx={{ width: '50%', marginTop: '5%' }}>
+          <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+            {steps.map((label, index) => (
+              <Step key={label} completed={completed[index]} sx={{ // Add styles specific to the steps
+                // Define your mobile-specific styles here
+                '@media (max-width: 600px)': {
+                    position: 'relative',
+                    right: '15%',
+                },
+              }}>
+                <StepButton
+                  color="inherit"
+                  onClick={handleStep(index)}
+                  icon={
+                    index === 0 ? (
+                      <WebIcon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
+                    ) : index === 1 ? (
+                      <PhotoshopIcon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
+                    ) : index === 2 ? (
+                      <Diversity1Icon fontSize="large" color={activeStep === index ? 'primary' : 'inherit'} />
+                    ) : null
+                  }
+                >
+                  {label}
+                </StepButton>
+              </Step>
+            ))}
+          </Stepper>
+          <div>
+            {allStepsCompleted() ? (
+              <React.Fragment>
+                <Typography sx={{ mt: 2, mb: 1 }}>
+                  All steps completed - you&apos;re finished
+                </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                  <Box sx={{ flex: '1 1 auto' }} />
+                  <Button onClick={handleReset}>Reset</Button>
+                </Box>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
+                  {stepMessages[activeStep]}
+                </Typography>
+              </React.Fragment>
+            )}
+          </div>
+        </Box>
       </Box>
-    </Box>
-  );
-}
+    );
+  }
+  
