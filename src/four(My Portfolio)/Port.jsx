f6c5e399904fc3946d21baeb947c6ffc2cd966cf
Manab Pokhrel <criroseven39@gmail.com>
2025-10-03 22:32:27 +0000

    git

--- src/four(My Portfolio)/Port.jsx
+++ src/four(My Portfolio)/Port.jsx
@@ -2,15 +2,11 @@
 import React from 'react';
 import './Port.css';
 import WebsitePreview from './WebsitePreview';
-import FlexboxGapStack from './Intro';
 
 const Port = () => {
   return (
-    <div className="port">
-      <FlexboxGapStack />
       <div className="prev">
         <WebsitePreview />
-      </div>
     </div>
   );
 };
