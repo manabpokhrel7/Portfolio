84be0596d56808204be560d0347b84cb9335d547
manabpokhrel7 <criroseven39@gmail.com>
2025-11-09 01:16:03 +0000

    git

--- src/footer/Contact.jsx
+++ src/footer/Contact.jsx
@@ -17,9 +17,6 @@ import {
 import { useForm, ValidationError } from "@formspree/react";
 import EmailIcon from "@mui/icons-material/Email";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { SiMedium } from "react-icons/si";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import "./Contact.css";
 
